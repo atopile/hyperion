5533ffe895d2a9a1e35aab1cdaeff85dab773085
Sam Mellor <sam@atopile.io>
2025-08-15 20:39:05 -0700

    checkerboard

--- firmware/hyperion/src/animations.cpp
+++ firmware/hyperion/src/animations.cpp
@@ -12,44 +12,106 @@ static constexpr int NUM_COLORS = 4;
 static constexpr int NUM_PIXELS = NUM_ROWS * NUM_COLS * NUM_COLORS;
 static constexpr int MAX_BRIGHTNESS = 0xFFFF;
 
+static constexpr int RED = 0;
+static constexpr int GREEN = 1;
+static constexpr int BLUE = 2;
+static constexpr int WHITE = 3;
+
+static constexpr rgbw_color_t BLACK = {0, 0, 0, 0};
+static constexpr rgbw_color_t WHITE = {0, 0, 0, MAX_BRIGHTNESS};
+static constexpr rgbw_color_t WHITE_WHITE = {MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTNESS};
+
+// Set a single pixel to an RGBW color
+void set_pixel(led_image_t &image, int row, int col, rgbw_color_t color)
+{
+    image[row][col][RED] = color.r;
+    image[row][col][GREEN] = color.g;
+    image[row][col][BLUE] = color.b;
+    image[row][col][WHITE] = color.w;
+}
+
+// Set all pixels to the same color
+void set_all_pixels(led_image_t &image, rgbw_color_t color)
+{
+    for (int row = 0; row < NUM_ROWS; row++)
+    {
+        for (int col = 0; col < NUM_COLS; col++)
+        {
+            set_pixel(image, row, col, color);
+        }
+    }
+}
+
+// Clear the entire image (set all channels to 0)
+void clear_image(led_image_t &image)
+{
+    set_all_pixels(image, BLACK);
+}
+
 // Pulse LEDs with a breathing effect
 void pulsing(float frequency)
 {
     led_image_t image;
 
     // Fade up
-    for (int brightness = 0; brightness < MAX_BRIGHTNESS; brightness += MAX_BRIGHTNESS / frequency)
+    for (uint16_t brightness = 0; brightness < MAX_BRIGHTNESS; brightness += MAX_BRIGHTNESS / frequency)
     {
-        // Set all pixels to the same brightness (white - all colors equal)
-        for (int row = 0; row < NUM_ROWS; row++)
-        {
-            for (int col = 0; col < NUM_COLS; col++)
-            {
-                for (int color = 0; color < NUM_COLORS; color++)
-                {
-                    image[row][col][color] = brightness;
-                }
-            }
-        }
+        set_all_pixels(image, WHITE_WHITE);
         set_image(image);
         sleep_ms(1000 / frequency);
     }
 
     // Fade down
-    for (int brightness = MAX_BRIGHTNESS; brightness > 0; brightness -= MAX_BRIGHTNESS / frequency)
+    for (uint16_t brightness = MAX_BRIGHTNESS; brightness > 0; brightness -= MAX_BRIGHTNESS / frequency)
     {
-        // Set all pixels to the same brightness (white - all colors equal)
-        for (int row = 0; row < NUM_ROWS; row++)
-        {
-            for (int col = 0; col < NUM_COLS; col++)
-            {
-                for (int color = 0; color < NUM_COLORS; color++)
-                {
-                    image[row][col][color] = brightness;
-                }
-            }
-        }
+        set_all_pixels(image, WHITE_WHITE);
         set_image(image);
         sleep_ms(1000 / frequency);
     }
+}
+
+// Checkerboard pattern that alternates between two colors
+void checkerboard_flash(rgbw_color_t color1, rgbw_color_t color2, uint32_t interval_ms)
+{
+    led_image_t image;
+
+    // Pattern 1: Start with color1 on even squares, color2 on odd squares
+    clear_image(image);
+    for (int row = 0; row < NUM_ROWS; row++)
+    {
+        for (int col = 0; col < NUM_COLS; col++)
+        {
+            // Checkerboard logic: (row + col) % 2 determines the pattern
+            if ((row + col) % 2 == 0)
+            {
+                set_pixel(image, row, col, color1);
+            }
+            else
+            {
+                set_pixel(image, row, col, color2);
+            }
+        }
+    }
+    set_image(image);
+    sleep_ms(interval_ms);
+
+    // Pattern 2: Swap colors - color2 on even squares, color1 on odd squares
+    clear_image(image);
+    for (int row = 0; row < NUM_ROWS; row++)
+    {
+        for (int col = 0; col < NUM_COLS; col++)
+        {
+            // Inverted checkerboard
+            if ((row + col) % 2 == 0)
+            {
+                set_pixel(image, row, col, color2);
+            }
+            else
+            {
+                set_pixel(image, row, col, color1);
+            }
+        }
+    }
+    set_image(image);
+    sleep_ms(interval_ms);
 }
--- firmware/hyperion/src/animations.h
+++ firmware/hyperion/src/animations.h
@@ -5,7 +5,20 @@
 #ifndef ANIMATIONS_H
 #define ANIMATIONS_H
 
+#include <stdint.h>
+
+// RGBW color structure
+typedef struct {
+    uint16_t r;
+    uint16_t g;
+    uint16_t b;
+    uint16_t w;
+} rgbw_color_t;
+
 // Pulse LEDs with a breathing effect
 void pulsing(float frequency);
+
+// Checkerboard pattern that alternates between two colors
+void checkerboard_flash(rgbw_color_t color1, rgbw_color_t color2, uint32_t interval_ms);
 
 #endif // ANIMATIONS_H
